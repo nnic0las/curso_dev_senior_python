702abad559fe677ee22a533c67ecb505cb0ff599
nnic0las <nielmepi2002@gmail.com>
2024-11-20 22:44:34 -0500

    Reto_1

---
+++ Reto_1/Reto_1.py
@@ -0,0 +1,52 @@
+#print("hola")        INVESTIGAR LA MAQUETACION
+#estructurar para que haya un buen funcionamiento
+#visualizacin print consola
+#atributos
+#atributos de un experimento: nombre, fecha, tipo de resultados
+LISTADeExperimentos = [
+    "experimento 1", "16/11/2024", "quimica", [5,3,4,5,6,44],
+]
+import datatime
+def agregarExperimento():
+    '''funcion permite un nuevo experimento con sus atributos'''
+    pass
+#permite eliminar un experimento,    
+def eliminarExperimento():
+ pass
+def visualizarExperimento():
+    #permite visualizar todos los experimentos, requiere el uso de la funcion agregarr experimento dificulatas 1 
+    pass
+ #calcular estadisticasbasicas, promedio maximos y minimos de un experimento, requiere el uso de funcion agrear experimento prioridad 2
+def calcularEstadistica():
+    pass
+
+def compararExperimento():
+    #compara 2 o mas experimentos para determinar los mejores o peores resultados, requiere el uso de calcular estadistica dificula 2
+   
+    pass
+def generarInforme():
+    #generar un informe resumido de los experimentos y sus estadisticas
+    pass
+#muestra el menu principal del programa
+#mejorar el menu 1
+#mejorar el informe por que re  quiere el uso de ufnciones y calcular para su uso
+def mostrarMenu():
+    print("=========Menu Principal==0=======")
+    print("=========Gestion de experimentos=========")
+    print("1. agregarexperimento")
+    print("2. visualizar expeimento")
+    print("3.calcular experimento")
+    print("=========Analisis de datos=========")
+    print("4.calcular estadisticas")
+    print("5.comparar experimentos")
+    print("=========informes=========")
+    print("6.Generar informe")
+    print("7. salir")
+    
+        
+    pass
+#funcion principal controla el flujo general del sistema
+def main():
+#conrola el flujo
+    pass
+main()
